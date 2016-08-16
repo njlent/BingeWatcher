e691647161faa4a6bad1e26ac4b669a67c03cbe4
njlent <design@njlent.de>
2016-08-16 18:53:12 +0200

    Feed include
    Moved feed to include

---
+++ code/include/content_feed.php
@@ -0,0 +1,197 @@
+<div class="content">
+            
+            <!-- spacer for menu -->
+            <div class="contentspacer"></div>
+            
+            <!-- global announcement -->
+            <div class="announcement contentbg"><a class="link" href="#announcements">
+                <img class="annicon" src="images/icon_ann.png" />
+                        
+                        <div class="anntext">Global Announcement!
+                            </div>
+                            
+                        <div class="anndesc">Server Maintenance: in 3 days (20.05.16)
+                            </div>
+                </a></div>
+                
+            <!-- request approved -->
+            <div class="announcement contentbg"><a class="link" href="#request">
+                <img class="annicon" src="images/icon_approved.png" />
+                        
+                        <div class="anntext">Request Approved!
+                            </div>
+                            
+                        <div class="anndesc">Mad Max - Fury Road ist nun in einer besseren Qualität verfügbar!
+                            </div>
+                </a></div>
+                
+            <!-- request denied -->
+            <div class="announcement contentbg"><a class="link" href="#request">
+                <img class="annicon" src="images/icon_denied.png" />
+                        
+                        <div class="anntext">Request Denied!
+                            </div>
+                            
+                        <div class="anndesc">X-Men Apocalypse kommt erst am 19.05.16 in die Kinos!
+                            </div>
+                </a></div>
+                
+            <!-- Series - Mr. Robot OT-->
+            <div class="series contentbg"><a class="link" href="#series">
+                <img class="sericon" src="images/series/mrrobot_small.png" />
+                        
+                        <div class="seriname">Mr. Robot <font class="blue">OT</font>
+                            </div>
+                            
+                        <div class="seridesc">New Series!
+                            </div>
+                </a>
+                
+                <!--eposiodes-->
+                        <div class="seriepisodes">
+                            
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E10</font> Zer0_Day
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E09</font> m1rr0r1ng
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E08</font> wh1ter0se
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E07</font> s0urce
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E06</font> br4ve-trave1er
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E05</font> 3xpl0its
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E04</font> da3m0ns
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E03</font> d3bug
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E02</font> ones-and-zer0es
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E01</font> Hellofriend
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            
+                            </div>
+                
+                </div>
+                
+                
+            <!-- Series - Game of Thrones-->
+            <div class="series contentbg"><a class="link" href="#series">
+                <img class="sericon" src="images/series/got_small.png" />
+                        
+                        <div class="seriname">Game of Thrones <font class="blue"></font>
+                            </div>
+                            
+                        <div class="seridesc">New Season!
+                            </div>
+                </a>
+                
+                <!--eposiodes-->
+                        <div class="seriepisodes">
+                            
+                            <a href="#episode" class="seriepisode"><font class="grey">S6E03</font> Oathbreaker
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/uhd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S6E02</font> Home
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/uhd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S6E01</font> The Red Woman
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/uhd.png"/>
+                            </a>
+                           
+                            
+                            </div>
+                
+                </div>
+                
+            <!-- Series - Mr. Robot-->
+            <div class="series contentbg"><a class="link" href="#series">
+                <img class="sericon" src="images/series/mrrobot_small.png" />
+                        
+                        <div class="seriname">Mr. Robot <font class="blue"></font>
+                            </div>
+                            
+                        <div class="seridesc">New Series!
+                            </div>
+                </a>
+                
+                <!--eposiodes-->
+                        <div class="seriepisodes">
+                            
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E10</font> Zer0_Day
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E09</font> m1rr0r1ng
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E08</font> wh1ter0se
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E07</font> s0urce
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E06</font> br4ve-trave1er
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E05</font> 3xpl0its
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E04</font> da3m0ns
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E03</font> d3bug
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E02</font> ones-and-zer0es
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            <a href="#episode" class="seriepisode"><font class="grey">S1E01</font> Hellofriend
+                            <img src="images/badges/new.png"/>
+                            <img src="images/badges/hd.png"/>
+                            </a>
+                            
+                            </div>
+                
+                </div>
+                
+           <!-- layout -->
+           <img style="width:100%;" src="images/background_feed.png">
+          
+                
+                
+                
+        </div>
+        <!-- content_end -->
--- code/include/menu.php
+++ code/include/menu.php
@@ -1,21 +1,23 @@
 <!-- header_start -->
 
 		<div class="headr">
+            <a href="#usermenu" class="usermenu_bttn"><img src="images/usermenu.png"/></a>
             <div class="headline red"><?php echo $page ?></div>
             <div class="headmenu">
-                <?php if ($page == feed) { ?><a href="feed.php" class="menuitem active">Feed</a>
+
+                <?php if ($page == Feed) { ?><a href="feed.php" class="menuitem active">Feed</a>
                 <?php } else { ?><a href="feed.php" class="menuitem">Feed</a><?php } ?>
 
-                <?php if ($page == music) { ?><a href="music.php" class="menuitem active">Movies</a>
+                <?php if ($page == Music) { ?><a href="music.php" class="menuitem active">Movies</a>
                 <?php } else { ?><a href="movies.php" class="menuitem">Movies</a><?php } ?>
 
-                <?php if ($page == series) { ?><a href="series.php" class="menuitem active">Series</a>
+                <?php if ($page == Series) { ?><a href="series.php" class="menuitem active">Series</a>
                 <?php } else { ?><a href="series.php" class="menuitem">Series</a><?php } ?>
 
-                <?php if ($page == music) { ?><a href="music.php" class="menuitem active">Music</a>
+                <?php if ($page == Music) { ?><a href="music.php" class="menuitem active">Music</a>
                 <?php } else { ?><a href="music.php" class="menuitem">Music</a><?php } ?>
 
-                <?php if ($page == request) { ?><a href="request.php" class="menuitem active">Request</a>
+                <?php if ($page == Request) { ?><a href="request.php" class="menuitem active">Request</a>
                 <?php } else { ?><a href="request.php" class="menuitem">Request</a><?php } ?>
 
                 <div class="menuspace">&nbsp;</div>
